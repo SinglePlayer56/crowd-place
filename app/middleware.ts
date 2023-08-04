2aa389924ddb0fb12a03da9fbf2e0a8901602381
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-08-04 21:05:35 +0500

    add middleware contacts

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,7 +3,7 @@ import {NextResponse} from "next/server";
 const legacyPrefixes = ['/contacts', '/blog']
 
 export default async function middleware(req:any) {
-    const { pathname } = req.nextUrl
+    const { pathname } = req.nextUrl;
 
     if (legacyPrefixes.some((prefix) => pathname.startsWith(prefix))) {
         return NextResponse.next()
@@ -12,7 +12,7 @@ export default async function middleware(req:any) {
     // apply trailing slash handling
     if (
         !pathname.endsWith('/') &&
-        !pathname.match(/((?!\.well-known(?:\/.*)?)(?:[^/]+\/)*[^/]+\.\w+)/)
+        !pathname.match(/((?!(?:\/.*)?)(?:[^/]+\/)*[^/]+\.\w+)/)
     ) {
         req.nextUrl.pathname += '/'
         return NextResponse.redirect(req.nextUrl)
