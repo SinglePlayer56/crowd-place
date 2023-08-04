189df860efed3d537bf0e11267216d5d14c3e217
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-08-04 21:17:33 +0500

    test middleware for platforms

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import {NextResponse} from "next/server";
+
+const legacyPrefixes = ['/contacts', '/blog']
+
+export default async function middleware(req:any) {
+    // const { pathname } = req.nextUrl;
+    //
+    // if (legacyPrefixes.some((prefix) => pathname.startsWith(prefix))) {
+    //     return NextResponse.next()
+    // }
+    //
+    // // apply trailing slash handling
+    // if (
+    //     !pathname.endsWith('/') &&
+    //     !pathname.match(/((?!(?:\/.*)?)(?:[^/]+\/)*[^/]+\.\w+)/)
+    // ) {
+    //     req.nextUrl.pathname += '/'
+    //     return NextResponse.redirect(req.nextUrl)
+    // }
+
+    if (req.nextUrl.pathname === '/platforms') {
+        return NextResponse.redirect(new URL('/platforms/', req.url))
+    }
+}
