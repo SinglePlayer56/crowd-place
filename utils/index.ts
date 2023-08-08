baf9c8597d801540e4058bbbc12da7e3fe40d828
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-08-08 05:03:55 +0500

    connect to database

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,11 @@
 import {Sequelize} from "sequelize";
 
 
-const sequelize = new Sequelize('crowd-platform', 'root', '', {
+const sequelize = new Sequelize('crowdplace_platform', 'crowdplace', '24Uqh8I$pLJK', {
     dialect: 'mysql',
-    host: 'localhost',
-    dialectModule: require('mysql2')
+    host: '82.202.167.53',
+    dialectModule: require('mysql2'),
+    port: 3306
 });
 
 export default sequelize;
