7b0278f39799a6809fd0bf715f518dc5870865f4
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-08-21 02:07:25 +0500

    test

---
+++ .next/server/chunks/467.js
@@ -0,0 +1,75 @@
+exports.id = 467;
+exports.ids = [467];
+exports.modules = {
+
+/***/ 5780:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3380, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 954, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 4638));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 5289));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 7935));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 1871));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 4463));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 1577));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 5693));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 110));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 572));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3280, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9274, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3349, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9708, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 3871))
+
+/***/ }),
+
+/***/ 9724:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"head": "platforms_head__Ftj2k",
+	"head__title": "platforms_head__title__r580h",
+	"head__text": "platforms_head__text__ldNQ_",
+	"whoCan": "platforms_whoCan__0fAsX",
+	"whoCan__title": "platforms_whoCan__title__IHIxh",
+	"links": "platforms_links__JRWEV",
+	"links__button": "platforms_links__button__ciu0o"
+};
+
+
+/***/ }),
+
+/***/ 2015:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ PlatformsLayout),
+/* harmony export */   metadata: () => (/* binding */ metadata)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(8374);
+
+
+const metadata = {
+    title: "Platforms | Crowd Place",
+    description: "Platforms | Crowd Place",
+    alternates: {
+        canonical: `${process.env.DOMAIN}/platforms/`
+    }
+};
+function PlatformsLayout({ children }) {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components__WEBPACK_IMPORTED_MODULE_1__/* .PageLayout */ .Xg, {
+        children: children
+    });
+}
+
+
+/***/ })
+
+};
+;
---
+++ .next/server/chunks/917.js
@@ -0,0 +1,28 @@
+exports.id = 917;
+exports.ids = [917];
+exports.modules = {
+
+/***/ 2917:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3380, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 954, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 4638));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 5289));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 7935));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 1871));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 4463));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 1577));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 5693));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 110));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 572));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3280, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9274, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3349, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9708, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 3871))
+
+/***/ })
+
+};
+;
