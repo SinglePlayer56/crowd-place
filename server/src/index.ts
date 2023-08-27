565d5418c76dbd086f8055dc91663ad0d965b9f2
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-08-27 13:53:58 +0500

    test

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,6 +23,15 @@ app.use(cors({
     allowedHeaders: ['Content-Type'], // Разрешенные заголовки
 }));
 
+app.use((req: any, res: Response, next) => {
+    // Исключаем пути Next.js из перенаправления
+    if (req.hostname === envVariable.DOMAIN && req.port === '3002' && !req.url.startsWith('/')) {
+        const redirectUrl = `http://${req.hostname}:3001${req.url}`;
+        return res.redirect(redirectUrl);
+    }
+    next();
+});
+
 // app.use((req: any, res: Response, next) => {
 //     // Перенаправляем запросы с порта 3002 на порт 3001
 //     if (req.hostname === envVariable.DOMAIN && req.port === '3002') {
@@ -32,10 +41,10 @@ app.use(cors({
 //     next();
 // });
 
-app.use('/', createProxyMiddleware({
-    target: 'http://1864875-cn27374.twc1.net:3001', // Порт, на котором запущен Express сервер
-    changeOrigin: true,
-}));
+// app.use('/', createProxyMiddleware({
+//     target: 'http://1864875-cn27374.twc1.net:3001', // Порт, на котором запущен Express сервер
+//     changeOrigin: true,
+// }));
 
 app.get('/api/get-related/:industry/', getRelatedHandler);
 
