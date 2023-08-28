63545a21358d1404e0d587e6c3e05359468220a2
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-08-28 21:47:41 +0500

    test connectDB

--- server/src/utils/connectDB.ts
+++ server/src/utils/connectDB.ts
@@ -1,15 +1,14 @@
 import {Sequelize} from "sequelize";
 import mysql2 from 'mysql2';
 
-const sequelize = new Sequelize('crowdplace_platform', 'crowdplace', '24Uqh8I$pLJK', {
+const sequelize = new Sequelize('crowdplace', 'root', '', {
     dialect: 'mysql',
-    host: '82.202.167.53',
+    host: 'localhost',
     dialectModule: mysql2,
     dialectOptions: {
         charset: 'utf8mb4',
         collate: 'utf8mb4_unicode_ci'
-    },
-    port: 3306
+    }
 });
 
 export default sequelize;
